36276f220fbfcadbd35d338c275d054f4daa6b16
Ath-ai <muhammadatheek29@gmail.com>
2024-06-23 05:34:45 +0530

    Update openvoice_app.py

--- openvoice_app.py
+++ openvoice_app.py
@@ -117,8 +117,8 @@ def predict(prompt, style, audio_file_pth, mic_file_path, use_mic, agree):
             None,
             None,
         )
-    if len(prompt) > 200:
-        text_hint += f"[ERROR] Text length limited to 200 characters for this demo, please try shorter text. You can clone our open-source repo and try for your usage \n"
+    if len(prompt) > 1000000:
+        text_hint += f"[ERROR] Text length limited to 300 characters for this demo, please try shorter text. You can clone our open-source repo and try for your usage \n"
         gr.Warning(
             "Text length limited to 200 characters for this demo, please try shorter text. You can clone our open-source repo for your usage"
         )
